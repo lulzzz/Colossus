1addaf2edc4f62557149d07605cdad9dd2eeeda9
Anduin Xue <anduin@aiursoft.com>
2018-02-28 13:12:42 +0800

    Create project.

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# Colossus
-Colossus is a file up-loader and downloader as an OSS example application.
+# Aiursoft.Colossus
+
+[![Build Status](https://travis-ci.org/AiursoftWeb/Developer.svg?branch=master)](https://travis-ci.org/AiursoftWeb/Developer)
+
+Colossus is a file uploader and downloader as an OSS example application.
+
+## How to run
+
+**We strongly recommend running this app on Windows 10 or Windows Server 2016**
+
+### Dependencies
+
+* SQL Server LocalDb
+* .NET Core 2.0 SDK
+
+### Run with command
+
+Please excuse the following commands in the project folder:
+
+    set ASPNETCORE_ENVIRONMENT Development
+    dotnet restore
+    dotnet ef database update
+    dotnet run
+
+### Run in Visual Studio
+
+Please install Visual Studio 2017 with .NET Core development kit.
+
+1. Double click the `.sln` file.
+2. Strike `F5`.
+
+## How to publish
+
+Please excuse the following commands in the project folder:
+
+    set ASPNETCORE_ENVIRONMENT Production
+    dotnet restore
+    dotnet ef database update
+    dotnet publish
+
+If you have IIS installed already, just config the web path to:
+
+    .\bin\Debug\netcoreapp2.0\publish
+
+## How to contribute
+
+There are many ways to contribute to the project: logging bugs, submitting pull requests, reporting issues, and creating suggestions.
+
+Even if you have push rights on the repository, you should create a personal fork and create feature branches there when you need them. This keeps the main repository clean and your personal workflow cruft out of sight.
+
+We're also interested in your feedback for the future of this project. You can submit a suggestion or feature request through the issue tracker. To make this process more effective, we're asking that these include more information to help define them more clearly.
---
+++ src/Startup.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Aiursoft.Colossus.Data;
+using Aiursoft.Colossus.Models;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Aiursoft.Pylon;
+
+namespace Aiursoft.Colossus
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ColossusDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DatabaseConnection")));
+
+            services.AddIdentity<ColossusUser, IdentityRole>()
+                .AddEntityFrameworkStores<ColossusDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+            services.AddMvc()
+                .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseAiursoftSupportedCultures();
+            app.UseAiursoftAuthenticationFromConfiguration(Configuration, "Colossus");
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
