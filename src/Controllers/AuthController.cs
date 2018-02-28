224cf98765f9c38a6d96cdcf6c8a3a04de8a47eb
Anduin Xue <anduin@aiursoft.com>
2018-02-28 14:09:41 +0800

    Enabled auto system.

---
+++ src/Controllers/AuthController.cs
@@ -0,0 +1,50 @@
+﻿using Aiursoft.Colossus.Data;
+using Aiursoft.Colossus.Models;
+using Aiursoft.Pylon;
+using Aiursoft.Pylon.Attributes;
+using Aiursoft.Pylon.Models.ForApps.AddressModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aiursoft.Colossus.Controllers
+{
+    [AiurRequireHttps]
+    public class AuthController : AiurController
+    {
+        public readonly UserManager<ColossusUser> _userManager;
+        public readonly SignInManager<ColossusUser> _signInManager;
+        public readonly ColossusDbContext _dbContext;
+
+        public AuthController(
+            UserManager<ColossusUser> userManager,
+            SignInManager<ColossusUser> signInManager,
+            ColossusDbContext _context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _dbContext = _context;
+        }
+
+        [AiurForceAuth(preferController: "", preferAction: "", justTry: false, register: false)]
+        public IActionResult GoAuth()
+        {
+            throw new NotImplementedException();
+        }
+
+        [AiurForceAuth(preferController: "", preferAction: "", justTry: false, register: true)]
+        public IActionResult GoRegister()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IActionResult> AuthResult(AuthResultAddressModel model)
+        {
+            await AuthProcess.AuthApp(this, model, _userManager, _signInManager);
+            return Redirect(model.state);
+        }
+    }
+}
